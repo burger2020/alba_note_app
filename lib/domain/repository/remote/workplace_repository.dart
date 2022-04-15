03f4b4b946e454ddcd42e0bc2874ff740d3cbdef
burger <blac2013@gmail.com>
2022-04-15 18:55:05 +0900

    workplace repository 추가

--- lib/domain/repository/remote/workplace_repository.dart
+++ lib/domain/repository/remote/workplace_repository.dart
@@ -1,4 +1,10 @@
-abstract class WorkplaceOfBossRepository {
+import 'package:albanote_project/data/repository/base_repository.dart';
+import 'package:albanote_project/domain/repository/local/local_shared_preferences.dart';
+import 'package:dio/src/dio.dart';
+
+abstract class WorkplaceOfBossRepository extends BaseRepository {
+  WorkplaceOfBossRepository(Dio dio, LocalSharedPreferences localSP) : super(dio, localSP);
+
   /// 일터 정보 조회
   Future getWorkplaceInfo(int memberId, int? workplaceId);
 
